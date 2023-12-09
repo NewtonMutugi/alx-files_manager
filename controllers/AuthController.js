7d71460c067fd7f43894fea079f42c556486aaf5
NewtonMutugi <newtonmut197@gmail.com>
2023-12-09 12:02:34 +0300

    controllers/UsersController.js: tried to fix

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,27 +1,24 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { uuid } from 'uuidv4';
-import sha1 from 'sha1';
+// import sha1 from 'sha1';
 import redisClient from '../utils/redis';
 
-const AuthController = {
-  async getConnect(res, req) {
-    const { authorization } = req.headers;
-    if (!authorization) {
-      res.code(401).json({ error: 'Unauthorized' });
-    }
+class AuthController {
+  static async getConnect(res, req) {
+    const { user } = req;
     const token = uuid();
-    const hash = sha1(token);
-    await redisClient.set(`auth_${hash}`, authorization, 24 * 60 * 60);
-    res.status(200).json({ token });
-  },
 
-  async getDisconnect(req, res) {
+    await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
+    res.status(200).json({ token });
+  }
+
+  static async getDisconnect(req, res) {
     const token = req.headers['x-token'];
     if (!token) {
       res.code(401).json({ error: 'Unauthorized' });
     }
     await redisClient.del(`auth_${token}`);
     res.status(204).send();
-  },
-};
+  }
+}
 export default AuthController;
